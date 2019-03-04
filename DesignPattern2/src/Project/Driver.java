8fc675a9a201f9f678a916f44f862f0106ae436f
Thomasraf <37896696+Thomasraf@users.noreply.github.com>
2019-03-04 15:48:33 +0800

    First Set Of Classes

---
+++ DesignPattern2/src/Project/Driver.java
@@ -0,0 +1,10 @@
+package Project;
+
+public class Driver {
+
+	public static void main(String[] args) {
+		selectAccount sa = new selectAccount();
+
+	}
+
+}
---
+++ DesignPattern2/src/Project/accountList.java
@@ -0,0 +1,11 @@
+package Project;
+import java.util.ArrayList;
+
+public class accountList {
+	
+	private static ArrayList<signingUp> accounts = new ArrayList<signingUp>();
+	
+	public void addAccount(signingUp sp) {
+		accounts.add(sp);
+	}
+}
