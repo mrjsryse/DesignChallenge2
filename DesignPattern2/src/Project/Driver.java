57f87c29a722429d5605254347403d206617d632
Thomasraf <thomas_cruz@dlsu.edu.ph>
2019-03-05 20:08:10 +0800

    Testing GitBash

--- DesignPattern2/src/Project/Driver.java
+++ DesignPattern2/src/Project/Driver.java
@@ -6,6 +6,8 @@ public class Driver {
 		//"com.mysql.jdbc.Driver","jdbc:mysql://112.211.95.65:3306/","superuser","kathyemir","swdespa"
 		
 		//jdbc:mysql://127.0.0.1:3306/
+		
+		//test adding for Git REEEE
 		String URL = "jdbc:mysql://localhost:3306/";
 		String USERNAME = "root";
 		String PASSWORD = "reee";
