d8d3f170bfa85d9b6f6852af2bbc777b87bad9d6
William Reyes <william_reyes@dlsu.edu.ph>
2019-03-18 20:27:08 +0800

    message

--- src/AddSong.java
+++ src/AddSong.java
@@ -1,5 +1,8 @@
 import java.awt.BorderLayout;
 import java.awt.EventQueue;
+import java.awt.event.ActionEvent;
+import java.awt.event.ActionListener;
+import java.io.File;
 
 import javax.swing.JFrame;
 import javax.swing.JPanel;
@@ -7,14 +10,18 @@ import javax.swing.border.EmptyBorder;
 import javax.swing.JLabel;
 import javax.swing.JTextField;
 import javax.swing.JButton;
+import javax.swing.JFileChooser;
 
 public class AddSong extends JFrame {
 
 	private JPanel contentPane;
-	private JTextField textFieldSongName;
+	private JTextField textFieldArtistName;
 	private JTextField textFieldAlbum;
 	private JTextField textFieldGenre;
-
+	JButton btnChooseFile, btnNewButton;
+	private JTextField textFieldYear;
+	String songName;
+	private JTextField textFieldChosenFile;
 	/**
 	 * Launch the application.
 	 */
@@ -42,36 +49,90 @@ public class AddSong extends JFrame {
 		setContentPane(contentPane);
 		contentPane.setLayout(null);
 		
-		JLabel lblSongName = new JLabel("Song Name");
-		lblSongName.setBounds(29, 34, 64, 14);
-		contentPane.add(lblSongName);
+		JLabel lblArtistName = new JLabel("Artist Name");
+		lblArtistName.setBounds(0, 140, 86, 14);
+		contentPane.add(lblArtistName);
 		
 		JLabel lblGenre = new JLabel("Genre");
-		lblGenre.setBounds(29, 91, 46, 14);
+		lblGenre.setBounds(29, 194, 46, 14);
 		contentPane.add(lblGenre);
 		
 		JLabel lblAlbum = new JLabel("Album");
-		lblAlbum.setBounds(29, 59, 46, 14);
+		lblAlbum.setBounds(29, 167, 46, 14);
 		contentPane.add(lblAlbum);
 		
-		textFieldSongName = new JTextField();
-		textFieldSongName.setBounds(103, 31, 86, 20);
-		contentPane.add(textFieldSongName);
-		textFieldSongName.setColumns(10);
+		textFieldArtistName = new JTextField();
+		textFieldArtistName.setBounds(87, 137, 86, 20);
+		contentPane.add(textFieldArtistName);
+		textFieldArtistName.setColumns(10);
 		
 		textFieldAlbum = new JTextField();
 		textFieldAlbum.setColumns(10);
-		textFieldAlbum.setBounds(103, 56, 86, 20);
+		textFieldAlbum.setBounds(87, 164, 86, 20);
 		contentPane.add(textFieldAlbum);
 		
 		textFieldGenre = new JTextField();
 		textFieldGenre.setColumns(10);
-		textFieldGenre.setBounds(103, 88, 86, 20);
+		textFieldGenre.setBounds(87, 191, 86, 20);
 		contentPane.add(textFieldGenre);
 		
-		JButton btnNewButton = new JButton("Add Song");
+		 btnNewButton = new JButton("Add Song");
+		 btnNewButton.addActionListener(new btn_Add());
 		btnNewButton.setBounds(306, 203, 89, 23);
 		contentPane.add(btnNewButton);
+		
+		 btnChooseFile = new JButton("Choose File");
+		 btnChooseFile.addActionListener(new btn_ChooseFile());
+		btnChooseFile.setBounds(33, 73, 140, 25);
+		contentPane.add(btnChooseFile);
+		
+		textFieldYear = new JTextField();
+		textFieldYear.setColumns(10);
+		textFieldYear.setBounds(87, 220, 86, 20);
+		contentPane.add(textFieldYear);
+		
+		JLabel lblYear = new JLabel("Year");
+		lblYear.setBounds(29, 221, 46, 14);
+		contentPane.add(lblYear);
+		
+		textFieldChosenFile = new JTextField();
+		textFieldChosenFile.setColumns(10);
+		textFieldChosenFile.setBounds(29, 40, 144, 20);
+		contentPane.add(textFieldChosenFile);
 	}
+	
+	class btn_ChooseFile implements ActionListener
+	 {		
+		 public void actionPerformed(ActionEvent e)
+		 {
+			 JFileChooser chooser = new JFileChooser();
+			 chooser.showOpenDialog(null);
+			 File f = chooser.getSelectedFile();
+			  songName = f.getName();
+		 }
+		 
+	 }
+	
+	class btn_Add implements ActionListener
+	 {		
+		 public void actionPerformed(ActionEvent e)
+		 {
 
+			 textFieldChosenFile.setText(songName);
+			 String artistName = textFieldArtistName.getText();
+			 String albumName = textFieldAlbum.getText();
+			 String genre = textFieldGenre.getText();
+			 String year = textFieldYear.getText();
+			 
+			 Song addedSong = new Song(songName, artistName, albumName, genre, year);
+			 
+			 SongList sList = new SongList();
+			 sList.addEvent(addedSong);
+			 int index = sList.getIndex(addedSong);
+			 
+			
+			 
+		 }
+		 
+	 }
 }
--- src/MusicPlaer.java
+++ src/MusicPlaer.java
@@ -2,10 +2,15 @@ import jaco.mp3.player.MP3Player;
 import java.awt.BorderLayout;
 import java.awt.EventQueue;
 
+
+
 import javax.swing.JFrame;
 import javax.swing.JPanel;
 import javax.swing.border.EmptyBorder;
+import javax.swing.DefaultListModel;
 import javax.swing.JButton;
+import javax.swing.JFileChooser;
+
 import java.awt.event.ActionListener;
 import java.io.File;
 import java.awt.event.ActionEvent;
@@ -15,7 +20,12 @@ import javax.swing.JLabel;
 
 public class MusicPlaer extends JFrame {
 
+	MP3Player mp3 = new MP3Player(new File("C:\\Users\\Nello Santos\\Desktop\\Music\\DecAve.mp3"));
 	private JPanel contentPane;
+	JButton btnPickPlaylist, btnPickSong, btnCreatePlaylist, btnUploadSong, btnEditSong, btnPlay, btnPause, btnNextSong, btnPreviousSong;
+	JList yourSongsList;
+	JTextPane txtpnSongNameGenre;
+	private JButton btnRefresh;
 
 	/**
 	 * Launch the application.
@@ -43,75 +53,60 @@ public class MusicPlaer extends JFrame {
 
 		setDefaultCloseOperation(JFrame.DISPOSE_ON_CLOSE);
 
-		setBounds(100, 100, 812, 649);
+		setBounds(100, 100, 1189, 813);
 		contentPane = new JPanel();
 		contentPane.setBorder(new EmptyBorder(5, 5, 5, 5));
 		setContentPane(contentPane);
 		contentPane.setLayout(null);
 		
-		JButton btnPlay = new JButton("Play");
-		btnPlay.addActionListener(new ActionListener() {
-			public void actionPerformed(ActionEvent e) {
-			mp3.play();
-			}
-		});
-		btnPlay.setBounds(314, 525, 89, 45);
+		 btnPlay = new JButton("Play");
+		btnPlay.addActionListener(new btn_Play());
+		btnPlay.setBounds(492, 681, 89, 45);
 		contentPane.add(btnPlay);
 		
-		JButton btnPause = new JButton("Pause");
-		btnPause.addActionListener(new ActionListener() {
-			public void actionPerformed(ActionEvent arg0) {
-				mp3.pause();
-			}
-		});
-		btnPause.setBounds(413, 525, 89, 45);
+		 btnPause = new JButton("Pause");
+		btnPause.addActionListener(new btn_Pause());
+		btnPause.setBounds(593, 681, 89, 45);
 		contentPane.add(btnPause);
 		
-		JButton btnNextSong = new JButton(">");
-		btnNextSong.setBounds(526, 525, 89, 45);
+		 btnNextSong = new JButton(">");
+		btnNextSong.setBounds(705, 681, 89, 45);
 		contentPane.add(btnNextSong);
 		
-		JButton btnPreviousSong = new JButton("<");
-		btnPreviousSong.setBounds(196, 525, 89, 45);
+		 btnPreviousSong = new JButton("<");
+		btnPreviousSong.setBounds(380, 681, 89, 45);
 		contentPane.add(btnPreviousSong);
 		
 		JList playlistList = new JList();
-		playlistList.setBounds(53, 94, 175, 358);
+		playlistList.setBounds(25, 93, 322, 558);
 		contentPane.add(playlistList);
 		
 		JList playlistSongList = new JList();
-		playlistSongList.setBounds(257, 94, 299, 170);
+		playlistSongList.setBounds(403, 139, 375, 224);
 		contentPane.add(playlistSongList);
 		
-		JButton btnPickPlaylist = new JButton("Pick Playlist");
+		 btnPickPlaylist = new JButton("Pick Playlist");
 		btnPickPlaylist.addActionListener(new ActionListener() {
 			public void actionPerformed(ActionEvent e) {
 			}
 		});
-		btnPickPlaylist.setBounds(53, 471, 89, 45);
+		btnPickPlaylist.setBounds(53, 664, 136, 45);
 		contentPane.add(btnPickPlaylist);
 		
-		JButton btnPickSong = new JButton("Add Song to Playlist");
-		btnPickSong.addActionListener(new ActionListener() {
-			public void actionPerformed(ActionEvent e) {
-				AddSong as = new AddSong();
-				as.setVisible(true);
-			}
-		});
-		btnPickSong.setBounds(590, 452, 190, 45);
+		 btnPickSong = new JButton("Add Song to Playlist");
+		btnPickSong.setBounds(898, 664, 190, 45);
 		contentPane.add(btnPickSong);
 		
-		JTextPane textPane = new JTextPane();
-		textPane.setBounds(257, 300, 290, 146);
-		textPane.setText("Song Name: \n"
-						+ "Genre: \n");
-		contentPane.add(textPane);
+		 txtpnSongNameGenre = new JTextPane();
+		txtpnSongNameGenre.setBounds(403, 405, 375, 209);
+		txtpnSongNameGenre.setText("Song Name: \r\nGenre: \r\n");
+		contentPane.add(txtpnSongNameGenre);
 		
 		JLabel lblSongInfo = new JLabel("Song Info");
-		lblSongInfo.setBounds(267, 275, 89, 14);
+		lblSongInfo.setBounds(403, 373, 95, 26);
 		contentPane.add(lblSongInfo);
 		
-		JButton btnCreatePlaylist = new JButton("Create Playlist");
+		 btnCreatePlaylist = new JButton("Create Playlist");
 		btnCreatePlaylist.addActionListener(new ActionListener() {
 			public void actionPerformed(ActionEvent e) {
 			}
@@ -119,20 +114,17 @@ public class MusicPlaer extends JFrame {
 		btnCreatePlaylist.setBounds(53, 11, 118, 45);
 		contentPane.add(btnCreatePlaylist);
 		
-		JButton btnUploadSong = new JButton("Upload Song");
-		btnUploadSong.addActionListener(new ActionListener() {
-			public void actionPerformed(ActionEvent e) {
-			}
-		});
-		btnUploadSong.setBounds(654, 11, 126, 45);
+		 btnUploadSong = new JButton("Upload Song");
+		btnUploadSong.addActionListener(new btn_UploadSong());
+		btnUploadSong.setBounds(1021, 11, 126, 45);
 		contentPane.add(btnUploadSong);
 		
-		JList yourSongsList = new JList();
-		yourSongsList.setBounds(580, 94, 194, 352);
+		 yourSongsList = new JList();
+		yourSongsList.setBounds(806, 94, 341, 557);
 		contentPane.add(yourSongsList);
 		
 		JLabel lblSongsInPlaylist = new JLabel("Songs in Playlist");
-		lblSongsInPlaylist.setBounds(257, 69, 95, 14);
+		lblSongsInPlaylist.setBounds(403, 112, 101, 26);
 		contentPane.add(lblSongsInPlaylist);
 		
 		JLabel lblPlaylists = new JLabel("Playlists");
@@ -140,15 +132,65 @@ public class MusicPlaer extends JFrame {
 		contentPane.add(lblPlaylists);
 		
 		JLabel lblYourSongs = new JLabel("Your Songs");
-		lblYourSongs.setBounds(587, 69, 95, 14);
+		lblYourSongs.setBounds(825, 69, 95, 14);
 		contentPane.add(lblYourSongs);
 		
-		JButton btnEditSong = new JButton("Edit Song");
+		 btnEditSong = new JButton("Edit Song");
 		btnEditSong.addActionListener(new ActionListener() {
 			public void actionPerformed(ActionEvent e) {
 			}
 		});
-		btnEditSong.setBounds(544, 11, 89, 45);
+		btnEditSong.setBounds(905, 11, 89, 45);
 		contentPane.add(btnEditSong);
+		
+		btnRefresh = new JButton("Refresh");
+		btnRefresh.addActionListener(new btn_Refresh());
+		btnRefresh.setBounds(760, 21, 97, 25);
+		contentPane.add(btnRefresh);
 	}
+	
+	 class btn_Play implements ActionListener 
+	 {
+
+	     public void actionPerformed(ActionEvent e) 
+	     {
+	        mp3.play();
+
+	     }
+	 }
+	 
+	 class btn_Pause implements ActionListener 
+	 {
+		 public void actionPerformed(ActionEvent e)
+		 {
+			 mp3.pause();
+		 }
+	 }
+	 
+	 class btn_UploadSong implements ActionListener
+	 {
+		 public void actionPerformed(ActionEvent e)
+		 {
+			 AddSong as = new AddSong();
+			 as.setVisible(true);
+			 
+		 }
+	 }
+	 
+	 class btn_Refresh implements ActionListener
+	 {
+		 public void actionPerformed(ActionEvent e)
+		 {
+			 SongList sList = new SongList();
+			 DefaultListModel DLM = new DefaultListModel();
+			 
+			 for(int x = 0; x < sList.getSongSize(); x++)
+			 DLM.addElement(sList.getSongList().get(x).ArtistName);
+			 
+			 yourSongsList.setModel(DLM);
+			 
+			 
+			 
+		 }
+	 }
 }
