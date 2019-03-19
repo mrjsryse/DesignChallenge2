f7e502731f7a9a3169f0b4668769ad01d14086c2
William Reyes <william_reyes@dlsu.edu.ph>
2019-03-19 22:44:32 +0800

    yay

--- src/SongPlan.java
+++
@@ -1,8 +0,0 @@
-
-public interface SongPlan { //Builder Interface
-	public void setSongName(String songName);
-	public void setSongArtistName(String artistName);
-	public void setSongAlbum(String albumName);
-	public void setSongGenre(String songGenre);
-	public void setSongYear(String year);
-}
