47a11d68e66215137f71cde9da57fca30fe5ca84
antonellosantos <42184482+antonellosantos@users.noreply.github.com>
2019-03-26 00:59:11 +0800

    Fixed Classes

--- src/view/AddSongDetails.java
+++ src/view/AddSongDetails.java
@@ -14,58 +14,63 @@ public class AddSongDetails extends JFrame{
 		getContentPane().setLayout(null);
 		
 		JLabel lblNewLabel = new JLabel("Song Details");
-		lblNewLabel.setFont(new Font("Tahoma", Font.PLAIN, 28));
 		lblNewLabel.setBounds(659, 31, 212, 76);
+		lblNewLabel.setFont(new Font("Tahoma", Font.PLAIN, 28));
 		getContentPane().add(lblNewLabel);
 		
 		JLabel lblSongTitle = new JLabel("Song Title");
-		lblSongTitle.setFont(new Font("Tahoma", Font.PLAIN, 24));
-		lblSongTitle.setBounds(39, 128, 116, 54);
+		lblSongTitle.setBounds(10, 71, 116, 54);
+		lblSongTitle.setFont(new Font("Tahoma", Font.PLAIN, 14));
 		getContentPane().add(lblSongTitle);
 		
 		SongTextField = new JTextField();
-		SongTextField.setBounds(39, 184, 403, 83);
+		SongTextField.setBounds(10, 113, 300, 30);
 		getContentPane().add(SongTextField);
 		SongTextField.setColumns(10);
 		
 		ArtistTextField = new JTextField();
+		ArtistTextField.setBounds(10, 165, 300, 30);
 		ArtistTextField.setColumns(10);
-		ArtistTextField.setBounds(39, 364, 403, 83);
 		getContentPane().add(ArtistTextField);
 		
 		JLabel lblArtist = new JLabel("Artist ");
-		lblArtist.setFont(new Font("Tahoma", Font.PLAIN, 24));
-		lblArtist.setBounds(39, 308, 116, 54);
+		lblArtist.setBounds(10, 136, 60, 32);
+		lblArtist.setFont(new Font("Tahoma", Font.PLAIN, 14));
 		getContentPane().add(lblArtist);
 		
 		GenreTextField = new JTextField();
+		GenreTextField.setBounds(10, 220, 300, 30);
 		GenreTextField.setColumns(10);
-		GenreTextField.setBounds(39, 547, 403, 83);
 		getContentPane().add(GenreTextField);
 		
 		JLabel lblGenre = new JLabel("Genre");
-		lblGenre.setFont(new Font("Tahoma", Font.PLAIN, 24));
-		lblGenre.setBounds(39, 491, 116, 54);
+		lblGenre.setBounds(10, 190, 68, 39);
+		lblGenre.setFont(new Font("Tahoma", Font.PLAIN, 14));
 		getContentPane().add(lblGenre);
 		
 		DateTextField = new JTextField();
+		DateTextField.setBounds(10, 275, 300, 30);
 		DateTextField.setColumns(10);
-		DateTextField.setBounds(39, 726, 403, 83);
 		getContentPane().add(DateTextField);
 		
 		JLabel lblDateMade = new JLabel("Date Made");
-		lblDateMade.setFont(new Font("Tahoma", Font.PLAIN, 24));
-		lblDateMade.setBounds(39, 670, 116, 54);
+		lblDateMade.setBounds(10, 250, 68, 31);
+		lblDateMade.setFont(new Font("Tahoma", Font.PLAIN, 14));
 		getContentPane().add(lblDateMade);
 		
 		AlbumTextField = new JTextField();
+		AlbumTextField.setBounds(10, 325, 300, 30);
 		AlbumTextField.setColumns(10);
-		AlbumTextField.setBounds(557, 184, 403, 83);
 		getContentPane().add(AlbumTextField);
 		
 		JLabel lblAlbum = new JLabel("Album");
-		lblAlbum.setFont(new Font("Tahoma", Font.PLAIN, 24));
-		lblAlbum.setBounds(557, 128, 116, 54);
+		lblAlbum.setBounds(10, 303, 57, 27);
+		lblAlbum.setFont(new Font("Tahoma", Font.PLAIN, 14));
 		getContentPane().add(lblAlbum);
+		
+		JLabel lblSongDetails = new JLabel("Song Details");
+		lblSongDetails.setFont(new Font("Tahoma", Font.BOLD, 18));
+		lblSongDetails.setBounds(194, 11, 116, 54);
+		getContentPane().add(lblSongDetails);
 	}
 }
