9c108664c5eec539c38c26e94b782d63f843ec6e
Thomasraf <thomas_cruz@dlsu.edu.ph>
2019-04-18 19:21:24 +0800

    Fixed the issue of creating a song

--- src/view/AddSong.java
+++ src/view/AddSong.java
@@ -151,8 +151,9 @@ public class AddSong extends JFrame {
 			 String genre = textFieldGenre.getText();
 			 String year = (String) yearComboBox.getSelectedItem();
 			 String path = textFieldChosenFile.getText();
-			 int count = 0;
 			 String favorite = "0";
+			 int count = 0;
+			 
 			 
 			 Song addedSong = new SongBuilder()
 					 .setSongID(songID)
@@ -164,7 +165,7 @@ public class AddSong extends JFrame {
 					 .setYear(year)
 					 .setPath(path)
 					 .setCount(count)
-					 .setFavorite(favorite);
+					 .setFavorite(favorite)
 					 .getSong();
 			 	     
 			 
